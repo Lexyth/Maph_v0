7abd2ec4f1ed69d4964638d317e8f4eb3f11dc21
Lexyth <Lexyth@users.noreply.github.com>
2022-04-26 23:36:55 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-alert("nothing");
+alert("loaded main.js");
+function setupOptions () {
+  let options = {"a", "b", "c", "d"};
+  let target = document.getElementById("target");
+  for (int i = 0; i < options.length; i++) {
+    target.appendChild(option);
+  }
+}
